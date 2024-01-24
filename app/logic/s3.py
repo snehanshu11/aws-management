fc037c9da518f66a5800c3eb16ac3706c86d6937
Snehanshu Suman <snehanshu.suman@khoros.com>
2024-01-24 20:55:11 +0530

    initial commit

---
+++ app/logic/s3.py
@@ -0,0 +1,22 @@
+import boto3
+from datetime import datetime
+# List objects in an amazon s3 bucket
+
+
+def list_buckets():
+    client = boto3.client('s3')
+    buckets= client.list_buckets()['Buckets']
+    return buckets
+
+def list_bucket_objects(bucket_name:str):
+    try:
+        client = boto3.resource('s3')
+        bucket = client.Bucket(bucket_name)
+        my_map =[]
+        for obj in bucket.objects.all():
+            my_map.append(obj.key)
+    except Exception:
+        print(f"[{datetime.utcnow()}]: Exception occured while getting bucket details:{bucket_name} not found.")
+    return my_map
+
+    
